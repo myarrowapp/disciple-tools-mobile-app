25de22ccf6dd16cef2f47fccbc540259bac51142
Andre Anmol <andre.anmol@rubicotech.in>
2025-08-14 19:33:36 +0530

    Update dependencies to Expo SDK 53 and React Native 0.79

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      "babel-preset-expo",
-      [
-        "@babel/preset-react",
-        {
-          importSource: "@welldone-software/why-did-you-render",
-          runtime: "automatic",
-          development: process.env.NODE_ENV === "development" || false,
-        },
-      ],
-    ],
+    presets: ["babel-preset-expo"],
     plugins: [
       "react-native-reanimated/plugin",
       [
